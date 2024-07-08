68eb9383a68271c41d221a8ac992987d93be0cf4
rhurtado29 <109316189+rhurtado29@users.noreply.github.com>
2024-07-07 22:40:41 -0600

    Proyecto Final Desarrollo Aplicaciones Web

---
+++ src/main/java/com/chconstructora/ChConstructoraApplication.java
@@ -0,0 +1,13 @@
+package com.chconstructora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChConstructoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChConstructoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chconstructora/ChConstructoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chconstructora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChConstructoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
